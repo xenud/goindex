314c3e9d76091d3a6f178ceb2d8e0e9cd6dad40d
Paolo Lazzaroni <lazzaronipaolo@outlook.it>
2020-03-12 18:31:43 +0100

    Update app.1.0.6-it.js

--- themes/material/app.1.0.6-it.js
+++ themes/material/app.1.0.6-it.js
@@ -352,6 +352,10 @@ function utc2beijing(utc_datetime) {
     timestamp = new Date(Date.parse(new_datetime));
     timestamp = timestamp.getTime();
     timestamp = timestamp/1000;
+	
+    // Aggiungi 8 ore, l'ora di Pechino è otto più fusi orari rispetto all'ora UTC
+    //var unixtimestamp = timestamp+8*60*60;
+    var unixtimestamp = timestamp;
 
     // Data e ora
     var unixtimestamp = new Date(unixtimestamp*1000);
