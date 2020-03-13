16da842aa3e543ce9a5019d9004e6b03b4006d73
Paolo Lazzaroni <lazzaronipaolo@outlook.it>
2020-03-13 11:42:37 +0100

    Update app.1.0.6-it.js

--- themes/material/app.1.0.6-it.js
+++ themes/material/app.1.0.6-it.js
@@ -372,9 +372,9 @@ function utc2beijing(utc_datetime) {
 
 // Conversione dei byte in KB, MB, GB
 function formatFileSize(bytes) {
-    if (bytes>=1000000000) {bytes=(bytes/1000000000).toFixed(2)+' GB';}
-    else if (bytes>=1000000)    {bytes=(bytes/1000000).toFixed(2)+' MB';}
-    else if (bytes>=1000)       {bytes=(bytes/1000).toFixed(2)+' KB';}
+    if (bytes>=1073741824) {bytes=(bytes/1073741824).toFixed(2)+' GB';}
+    else if (bytes>=1048576)    {bytes=(bytes/1048576).toFixed(2)+' MB';}
+    else if (bytes>=1024)       {bytes=(bytes/1024).toFixed(2)+' KB';}
     else if (bytes>1)           {bytes=bytes+' bytes';}
     else if (bytes==1)          {bytes=bytes+' byte';}
     else                        {bytes='';}
